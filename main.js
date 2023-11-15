f4e58e079de6d9a41fb7dface3f07ac1a47e256c
t-coffe <tcoffe@edenschool.fr>
2023-11-15 16:24:03 +0100

    first commit. challenges 1, 2, 3, 4, 5, 6

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const oneToTen = document.querySelector(".oneToTen p");
-let unADix = " ";
+let UNaDix = " ";
 for (let i = 0; i <= 10; i++) {
-  unADix = unADix + " " + i;
-  oneToTen.textContent = unADix;
+  UNaDix = UNaDix + " " + i;
+  oneToTen.textContent = UNaDix;
 }
 
 const oddMinusHundred = document.querySelector(".oddMinusHundred p");
 let impairCent = " ";
-for (let i = 0; i <= 100; i=i+2) {
+for (let i = 1; i <= 100; i=i+2) {
   impairCent = impairCent + " " + i;
   oddMinusHundred.textContent = impairCent;
 }
@@ -17,4 +17,33 @@ let gardienSept = " ";
 for (let i = 0; i <= 140; i=i+7) {
   gardienSept = gardienSept + " " + i;
   tableSept.textContent = gardienSept;
+}
+
+const tablesUNEaDix = document.querySelector(".tablesUNEaDix p");
+let garDix = "";
+for (let i = 1; i <= 10; i++) {
+  for (let j = 0; j <= 10; j++) {
+    garDix = garDix + " " + j*i;
+    tablesUNEaDix.textContent = garDix;
+  }
+}
+
+const sommeUNaDix = document.querySelector(".sommeUNaDix p");
+let plusUnAdix = 0;
+for (let i = 1; i <= 10; i++) {
+  plusUnAdix = plusUnAdix + i;
+  sommeUNaDix.textContent = plusUnAdix;
+}
+
+const produitUNaDix = document.querySelector(".produitUNaDix p");
+let foisUnAdix = 1;
+for (let i = 1; i <= 10; i++) {
+  foisUnAdix = foisUnAdix * i;
+  produitUNaDix.textContent = foisUnAdix;
+}
+
+const celsiusToFahrenheit = document.querySelector(".celsiusToFahrenheit p");
+
+const celToFarh = (n)=> {
+  
 }
